d119cf61f28aa459eb4615aeac8942e77c2668bf
Tazrian Halim <95393618+TazrianH@users.noreply.github.com>
2024-10-16 17:26:10 -0400

    restart deployment

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import os
 import random
 import json
 
-
 # Reference: https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
 st.title('Chat Bot Demo')
 st.subheader("Powered by Amazon Berock with Anthropic Claude v2",
