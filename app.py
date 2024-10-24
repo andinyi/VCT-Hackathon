99f876788267be2fa52d6fa477ef8bdfb0a7eafd
Andy <32392170+andinyi@users.noreply.github.com>
2024-10-24 05:19:53 -0400

    updating app

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
+st.logo("pumpkinLong.png", size='large', link="http://jettreviveme.com/", icon_image="pumpkin.png")
+
 About_page = st.Page("About.py", title='About', icon=':material/dashboard:')
+Source_page = st.Page("sources.py", title='Sources', icon=':material/view_timeline:')
 App_page = st.Page("app-chatbot.py", title='App', icon=':material/chat:')
 
-pg = st.navigation([About_page, App_page])
+pg = st.navigation({
+    "Information and About" : [About_page, Source_page],
+    "WebApp" : [App_page]
+})
 
 pg.run()
